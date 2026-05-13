e7abc968d373afa2753d5123001ecbf85c5a5622
sandrolorenzo88-del <sandrolorenzo88@gmail.com>
2026-05-13 11:07:48 +0200

    Create Message model for database

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.String(50), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
