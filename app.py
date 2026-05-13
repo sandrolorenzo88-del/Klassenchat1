2cf7d05970e1b3c771b9b553b44c6d7aa0079571
sandrolorenzo88-del <sandrolorenzo88@gmail.com>
2026-05-13 12:11:42 +0200

    Refactor SocketIO initialization and remove eventlet
    Removed eventlet monkey patching and initialized SocketIO with gevent.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-import eventlet
-
-eventlet.monkey_patch()
+socketio = SocketIO(
+    app,
+    cors_allowed_origins='*',
+    async_mode='gevent'
+)
 
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
