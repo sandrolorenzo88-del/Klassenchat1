e08557e5c420cc39c6f2fc0d84a1b5f7b3bae4dd
sandrolorenzo88-del <sandrolorenzo88@gmail.com>
2026-05-13 11:13:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from models import db, Message
+from datetime import datetime
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "secret"
 
 socketio = SocketIO(app, cors_allowed_origins="*")
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 participants = []
 messages = []
-
+user_sessions = {}
 @app.route("/")
 def index():
     return render_template("chat.html")
@@ -16,6 +25,7 @@ def index():
 @socketio.on("join")
 def handle_join(data):
     username = data["username"]
+    user_sessions[request.sid] = username
 
     if username not in participants:
         participants.append(username)
@@ -52,18 +62,51 @@ def handle_leave(data):
 @socketio.on("send_message")
 def handle_message(data):
     message = {
-        "sender": data["sender"],
-        "text": data["text"]
+    "sender": data["sender"],
+    "text": data["text"],
+    "time": datetime.now().strftime('%H:%M')
     }
 
-    messages.append(message)
+    new_message = Message(
+    sender=data["sender"],
+    text=data["text"]
+)
+
+db.session.add(new_message)
+db.session.commit()
 
     emit("message", message, broadcast=True)
 
 @socketio.on("get_state")
 def get_state():
+    all_messages = Message.query.order_by(Message.created_at.asc()).all()
+
+    history = []
+
+    for msg in all_messages:
+        history.append({
+            "sender": msg.sender,
+            "text": msg.text
+        })
+
     emit("participants", participants)
-    emit("message_history", messages)
+    emit("message_history", history)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    username = user_sessions.get(request.sid)
+
+    if username and username in participants:
+        participants.remove(username)
+
+        emit("participants", participants, broadcast=True)
+
+        system_message = {
+            "sender": "System",
+            "text": f"{username} hat die Verbindung getrennt."
+        }
+
+        emit("message", system_message, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
